e425b5c7e59c6cafd1f8ba202b3bec9295751c79
Vitradisa Pratama <vitradisapratama@gmail.com>
2024-05-16 15:04:28 +0700

    update camera quality

--- streamcam.py
+++ streamcam.py
@@ -1,16 +1,25 @@
 import cv2
 from ffmpeg import input, output, run_async
 
-rtmp_server_url = "rtmp://localhost:1935"
+rtmp_server_url = "rtmp://192.168.150.90:1935"  # Use rtmp protocol instead of http
+
+# Set camera settings
+camera_width = 1280
+camera_height = 720
 
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, camera_width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, camera_height)
 
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+# Set camera properties to improve quality
+cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)  # Disable autofocus
+cap.set(cv2.CAP_PROP_FOCUS, 0)  # Set focus to 0 (manual focus)
+cap.set(cv2.CAP_PROP_BRIGHTNESS, 50)  # Adjust brightness
+cap.set(cv2.CAP_PROP_CONTRAST, 50)  # Adjust contrast
 
 process = (
-    input('pipe:', format='rawvideo', pix_fmt='bgr24', s='640x480')
-    .output(rtmp_server_url, format='flv')
+    input('pipe:', format='rawvideo', pix_fmt='bgr24', s=f'{camera_width}x{camera_height}')
+    .output(rtmp_server_url, format='flv', vcodec='libx264', crf=18)  # Use H.264 encoding and adjust quality
     .run_async(pipe_stdin=True)
 )
 
