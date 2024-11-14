0790acef1e45082885bdcd06c9005798999b764d
Vitradisa Pratama <vitradisapratama@gmail.com>
2024-11-15 01:00:18 +0700

    update scripts

--- runcamera.bat
+++ runcamera.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 
 :: Activate the Python virtual environment
 call yolocamrmq/Scripts/activate
@@ -8,5 +8,3 @@ start cmd /k python yolocamrmq.py 0 camera-front
 start cmd /k python yolocamrmq.py 1 camera-side-1
 start cmd /k python yolocamrmq.py 2 camera-side-2
 start cmd /k python yolocamrmq.py 3 camera-back
-
-:: Run cartesian program
--- runcamera_stream.bat
+++ runcamera_stream.bat
@@ -1,5 +1,9 @@
 @echo on
 
+:: Activate the Python virtual environment
+call yolocamrmq/Scripts/activate
+
+:: Start the Python scripts in new command prompt windows
 start cmd /k python streamcam.py 0 rtmp://localhost:1935/live/camera-front
 start cmd /k python streamcam.py 1 rtmp://localhost:1935/live/camera-side-1
 start cmd /k python streamcam.py 2 rtmp://localhost:1935/live/camera-side-2
